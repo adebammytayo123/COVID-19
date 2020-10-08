ec3f7c9106e1db036982815e214670a4a4c71a61
adebammytayo123 <bamianike@gmail.com>
2020-10-08 08:10:06 -0700

    deleted and added some images

--- src/components/Contagion/Contagion.js
+++ src/components/Contagion/Contagion.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Contagion.css";
 import contact from './contact.jpg';
-import handshake from './handshake.jpg';
+import disinfecting from './disinfecting.jpg';
 import airtransmission from './airtransmission.webp';
 
 const Contagion = () => {
@@ -14,18 +14,18 @@ const Contagion = () => {
             <div className="contagion-container">
                 <div className="contagion-method">
                     <img className="img1" src={contact} alt="" />
-                    <h1 className="title">physical contact</h1>
-                    <p className="para">A mother to child during pregnancy, labor, or nursing</p>
+                    <h1 className="title">Human contact</h1>
+                    <p className="para">Physical contact with an infected person, therefore washing your hands is the simplest way you can protect yourself.</p>
                 </div>
                 <div className="contagion-method">
-                    <img className="img2" src={handshake} alt="" />
-                    <h1 className="title">handshake</h1>
-                    <p className="para">Having unprotected vaginal, anal, or oral sex.</p>
+                    <img className="img2" src={disinfecting} alt="" />
+                    <h1 className="title">Contaminated Surfaces</h1>
+                    <p className="para">Touching contaminated surfaces without disinfecting them first and using the same hands to rub yor face or nose.</p>
                 </div>
                 <div className="contagion-method">
                     <img className="img3" src={airtransmission} alt="" />
-                    <h1 className="title">ait transmission</h1>
-                    <p className="para">Using unclean needles and unscreen bloods for patients</p>
+                    <h1 className="title">air transmission</h1>
+                    <p className="para">Research shows that the virus can live in the air for up to 3 hours. It can get into your lungs if someone who has it breathes out and you breathe that air in.</p>
                 </div>
             </div>
         </div>
